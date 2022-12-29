cdf599337f4f27ccf4ec31dcd996c34c28fb17f8
zeyadomar93 <zeyad@graduate.utm.my>
2022-12-29 20:32:36 +0800

    added background

--- main.cpp
+++ main.cpp
@@ -17,36 +17,39 @@ int main()
 
 	Display *room;
 
-	initwindow(screenWidth, screenHeight, "Tutorial 7-Associations");
+	initwindow(screenWidth, screenHeight, "Group ERD");
+	readimagefile("highres2.jpg",0,0,1380,750);
 
-	Display rooms[] = {
-		Display(0, 0, screenWidth / 2, screenHeight, COLOR(255, 0, 0)),
-		Display(screenWidth / 2, 0, screenWidth / 2, screenHeight, COLOR(0, 0, 255))};
+	// Display rooms[] = {
+	// 	// Display(0, 0, screenWidth / 2, screenHeight, COLOR(255, 0, 0)),
+	// 	// Display(screenWidth / 2, 0, screenWidth / 2, screenHeight, COLOR(0, 0, 255))};
 
-	Ball balls[] = {
-		Ball(Point(100, screenHeight / 4), 100, YELLOW, 50, 50),
-		Ball(Point(100, screenHeight / 4), 150, RED, 50, -100)};
+	// Ball balls[] = {
+	// 	Ball(Point(100, screenHeight / 4), 100, YELLOW, 50, 50),
+	// 	Ball(Point(100, screenHeight / 4), 150, RED, 50, -100)};
 
-	Point p = rooms[0].getCenter();
-	balls[0].setLocation(p);
-	balls[1].setLocation(p);
+	// Point p = rooms[0].getCenter();
+	// balls[0].setLocation(p);
+	// balls[1].setLocation(p);
 
-	balls[0].setDisplay(rooms);
-	balls[1].setDisplay(rooms);
+	// balls[0].setDisplay(rooms);
+	// balls[1].setDisplay(rooms);
 
-	for (int r = 0; r < 2; r++)
-		rooms[r].draw();
+	// for (int r = 0; r < 2; r++)
+	// 	rooms[r].draw();
 
-	for (int b = 0; b < 2; b++)
-		balls[b].draw();
+	// for (int b = 0; b < 2; b++)
+	// 	balls[b].draw();
 
 	while (!kbhit())
 	{
-		delay(125);
+		delay(8000);
 
-		for (int b = 0; b < 2; b++)
-			balls[b].move();
+		for (int b = 0; b < 2; b++){
+			// balls[b].move();
+			int xf=5;
 	}
 
 	return 0;
+}
 }
