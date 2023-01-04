beac0be35147d8297fdd5939156034b6dd57554f
zeyadomar93 <zeyad@graduate.utm.my>
2023-01-04 13:09:40 +0800

    fixes

--- life.hpp
+++ life.hpp
@@ -1,32 +1,29 @@
 // #include "display.hpp"
 
+class Life
+{
+private:
+    int life;
+    int initial_life = 3;
 
-
-
-class Life {
-  private:
-    int life;  
-    int initial_life =3;
-
-  public:
- 
-    Life(int initial_life) 
+public:
+    Life(int initial_life)
     {
         life = initial_life;
     }
 
-    
-    void decrease_life() {
-      life -= initial_life;
+    void decrease_life()
+    {
+        life -= initial_life;
     }
 
-
     // method to display the current value of the life attribute
-    int display_life() {
-      return life;
+    int display_life()
+    {
+        return life;
     }
 };
 
-// Life player_life(3);  
+// Life player_life(3);
 // player_life.decrease_life();  // decrease the player's life by 1
 // player_life.display_life();  // display the current life of the player
--- main.cpp
+++ main.cpp
@@ -1,55 +1,36 @@
 
 #include <graphics.h>
-#include "ball.hpp"
 #include "display.hpp"
 #include "life.hpp"
-
-// Task 5:
-//    Review the main function before running.
 
 int main()
 {
 	int screenWidth = getmaxwidth();
 	int screenHeight = getmaxheight();
 
-	Display *room;
+	// Display *room;
 
 	initwindow(screenWidth, screenHeight, "Group ERD");
-Display display;
-Life life(3);
-display.drawLife(life.display_life());
-// display.draw();
-
-	// Display rooms[] = {
-	// 	// Display(0, 0, screenWidth / 2, screenHeight, COLOR(255, 0, 0)),
-	// 	// Display(screenWidth / 2, 0, screenWidth / 2, screenHeight, COLOR(0, 0, 255))};
-
-	// Ball balls[] = {
-	// 	Ball(Point(100, screenHeight / 4), 100, YELLOW, 50, 50),
-	// 	Ball(Point(100, screenHeight / 4), 150, RED, 50, -100)};
-
-	// Point p = rooms[0].getCenter();
-	// balls[0].setLocation(p);
-	// balls[1].setLocation(p);
-
-	// balls[0].setDisplay(rooms);
-	// balls[1].setDisplay(rooms);
-
-	// for (int r = 0; r < 2; r++)
-	// 	rooms[r].draw();
-
-	// for (int b = 0; b < 2; b++)
-	// 	balls[b].draw();
+	Display display;
+	Life life(3);
+	display.draw();
+	// display.drawLife(life.display_life());
 
 	while (!kbhit())
-	{
-		delay(8000);
+        {
 
-		for (int b = 0; b < 2; b++){
-			// balls[b].move();
-			int xf=5;
-	}
+delay(10000);
 
-	return 0;
-}
+        }
+			int key;
+            key = getch();
+
+            if (key == 0) 
+                key = getch();
+            switch (toupper(key))
+            {
+            case 'q':
+
+                break;
+            }
 }
